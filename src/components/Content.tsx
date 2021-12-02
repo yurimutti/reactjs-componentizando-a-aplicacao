4d87b1d2f61962f9f6df9cbf8af27cf8fe77d77c
Yuri Mutti <yuri.cruz@enext.com.br>
2021-12-01 21:00:15 -0300

    feat(componentes): separate app in componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+export function Content({ movies, selectedGenre }) {
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre: any) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
